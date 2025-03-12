5107d84c2b7a3ebf8f69d62f2d5d5e6408fc9a7b
Jableed43 <javiernehuenlopez@gmail.com>
2025-03-12 17:40:41 -0300

    test-vercel deploy clean

--- api/index.js
+++ api/index.js
@@ -3,11 +3,8 @@ import { connectDB } from "../src/db.js";
 import userRouter from "../src/routes/userRoute.js";
 import taskRouter from "../src/routes/taskRoute.js";
 import cors from "cors";
-import { PORT as SERVERPORT } from "../src/config.js";
-
 
 const app = express();
-const PORT = process.env.NODE_ENV === "test" ? 0 : SERVERPORT;
 
 app.use(cors({
   origin: '*',
@@ -24,23 +21,6 @@ app.get("/api/test", (req, res) => {
   res.status(200).send("working ok");
 });
 
-
 await connectDB();
 
-const startServer = async () => {
-  const server = app.listen(PORT, () => {
-  });
-  console.log(`Server is running on port ${server.address().port}`);
-
-  process.on("SIGINT", () => {
-    server.close(() => {
-      process.exit(0);
-    });
-  });
-
-  return server;
-};
-
-startServer();
-
-export default app
+export default app;
